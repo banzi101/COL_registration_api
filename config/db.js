db17a86f6689b9bf550eb6b0d11ec043a758e759
banu <banugodell@gmail.com>
2024-08-09 14:57:24 +1000

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDb = async () => {
   try {
     const connect = await mongoose.connect(
-      `mongodb+srv://banu:nXCaJ97IchsJ9KoV@cluster0.oobgic4.mongodb.net/musicModule?retryWrites=true&w=majority&appName=Cluster0`
+      `***`
     );
     console.log(
       "DB connected",
